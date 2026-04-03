b4e8c4cc3045c42ddebaaf117523d672ad122e0e
clebson.araujo <clebson.araujo.25@gmail.com>
2026-04-03 15:59:59 -0300

    Adicionando jogador

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,19 +8,28 @@ class Play extends Phaser.Scene {
     create() {
         const map = this.createMap();
         const layers = this.createLayers(map);
+
+        this.createPlayer();
     }
 
     createMap() {
-        const map = this.make.tilemap({key: 'map'});
-        map.addTilesetImage('main_lev_build_1', 'tiles-1');
+        const map = this.make.tilemap({key: "map"});
+        map.addTilesetImage("main_lev_build_1", "tiles-1");
         return map;
     }
 
     createLayers(map) {
-        const tileset = map.getTileset('main_lev_build_1');
-        const environment = map.createStaticLayer('environment', tileset);
-        const platforms = map.createDynamicLayer('platforms', tileset);
+        const tileset = map.getTileset("main_lev_build_1");
+        const environment = map.createStaticLayer("environment", tileset);
+        const platforms = map.createDynamicLayer("platforms", tileset);
+
         return { environment, platforms };
+    }
+
+    createPlayer() {
+        const player = this.physics.add.sprite(100, 250, "player");
+        player.body.setGravityY(500);
+        player.setCollideWorldBounds(true);
     }
 }
 
